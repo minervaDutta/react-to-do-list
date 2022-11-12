74305fbcac45ae320f43a3b8264e421f9fbd12d0
Minerva Dutta <dutta.minerva@gmail.com>
2022-11-12 09:44:14 -0800

    added code suring class

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+// h1 = todos
+
+// div =
+//   input
+//   a list (ul)
+//     list items of todos (li)
+//        text (p, label, whatver)
+//        a button to delete
 const Home = () => {
+	const [ToDos, setToDos] = useState(["a","b","c"]);
+	// const[inputValue, setInputValue]=useState("");
+	// setting state
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<h1>Todos </h1>
+			<div>
+				{/* if enter
+				add todo
+				refresh input
+				else
+				setInputvalue to new value */}
+				<input onKeyUp={(event)=>{
+					if(event.code=="Enter"){
+						let newToDos= [...ToDos];
+						newToDos.push(event.target.value);
+						setToDos(newToDos);
+					}
+					else {
+						// setInputValue(event.target.value);
+						// console.log(inputValue);
+					}
+					console.log(event);
+				}}/>
+				<ul>
+					{ToDos.map(
+					(todo, index)=>{
+						return(<li>
+							<p>{todo}</p>
+							<button onClick={()=>{
+								let newToDos= [...ToDos];
+								newToDos.splice(index,1);
+								setToDos(newToDos);
+							}}>x</button>
+							</li>)
+					}
+					)}
+				</ul>
+				
+			</div>
 			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+			{ToDos.length}
 			</p>
 		</div>
 	);
 };
 
 export default Home;
+
+//Things left to-do
+// Empty the input on enter
+// Add placeholder to input
+// Style todos
+// Save todos across browser refresh
